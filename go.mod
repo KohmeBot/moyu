c69f9d918d251b79ef50e400745df05557c4489b
jhue <threeax@foxmail.com>
2025-11-27 15:08:41 +0800

    feat: 适应plugin v2

--- go.mod
+++ go.mod
@@ -1,10 +1,12 @@
 module github.com/kohmebot/moyu
 
-go 1.23.1
+go 1.24.0
+
+toolchain go1.24.2
 
 require (
 	github.com/kohmebot/pkg v1.0.2
-	github.com/kohmebot/plugin v1.2.5
+	github.com/kohmebot/plugin/v2 v2.0.1
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/wdvxdr1123/ZeroBot v1.8.1
 )
@@ -13,14 +15,14 @@ require (
 	github.com/FloatTech/ttl v0.0.0-20250224045156-012b1463287d // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/kohmebot/chatai v0.0.57 // indirect
+	github.com/kohmebot/chatai v0.1.0 // indirect
 	github.com/panjf2000/ants/v2 v2.11.3 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/tidwall/gjson v1.18.0 // indirect
-	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/match v1.2.0 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
-	golang.org/x/sync v0.16.0 // indirect
-	golang.org/x/sys v0.34.0 // indirect
-	golang.org/x/text v0.27.0 // indirect
-	gorm.io/gorm v1.30.1 // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+	gorm.io/gorm v1.31.1 // indirect
 )
--- moyu/plugin.go
+++ moyu/plugin.go
@@ -5,7 +5,7 @@ import (
 	"github.com/kohmebot/chatai/chatai/chataisdk"
 	"github.com/kohmebot/pkg/command"
 	"github.com/kohmebot/pkg/version"
-	"github.com/kohmebot/plugin"
+	"github.com/kohmebot/plugin/v2"
 	"github.com/robfig/cron/v3"
 	"github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
@@ -25,7 +25,7 @@ func NewPlugin() plugin.Plugin {
 	return new(PluginMoyu)
 }
 
-func (p *PluginMoyu) Init(engine *zero.Engine, env plugin.Env) error {
+func (p *PluginMoyu) OnInit(engine plugin.Engine, env plugin.Env) error {
 	err := env.GetConf(&p.conf)
 	if err != nil {
 		return err
@@ -76,23 +76,20 @@ func (p *PluginMoyu) Name() string {
 	return "moyu"
 }
 
-func (p *PluginMoyu) Description() string {
-	return "每日发送摸鱼日历"
+func (p *PluginMoyu) Version() string {
+	return "v0.1.0"
 }
 
-func (p *PluginMoyu) Commands() fmt.Stringer {
-	return command.NewCommands()
-}
+func (p *PluginMoyu) OnHelp(ctx *zero.Ctx) {
 
-func (p *PluginMoyu) Version() uint64 {
-	return uint64(version.NewVersion(0, 0, 28))
 }
 
 func (p *PluginMoyu) OnBoot() {
 	sendErr := func(err error) {
-		for ctx := range p.env.RangeBot {
+		p.env.UseBot(func(ctx *zero.Ctx) {
 			p.env.Error(ctx, err)
-		}
+		})
+
 	}
 	c := cron.New()
 	_, err := c.AddFunc(p.conf.SendCron, func() {
